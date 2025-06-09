5bec892d4bec2fbd09e333eed5a01d13975044a1
Aasthasingh09-dotcom <aasthasinghrajput09@gmail.com>
2025-06-09 13:20:02 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,5 @@
+n=int(input('enter a number'))
+from math import *
+print ( 'square root of number is =',sqrt(n))
+print ('natural logarithm of a number is =',log(n))
+print ( 'sine of number is =',sin(n))
